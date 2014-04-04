7cdf29e7abba3ae20790cc45d1581278736174f0
Tomas Raila <tomas.raa@gmail.com>
2014-04-04 18:22:38 +0300

    added view method to extract and return image of single volcano instance from specified image

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ import re
 from app import app
 from flask import abort, render_template, make_response
 
-from config import IMAGE_DIR, GROUND_TRUTH_DIR
+from config import IMAGE_DIR, GROUND_TRUTH_DIR, VOLCANO_PIXEL_SIZE
 from lib.utils.reader import list_signals, read_signal, read_lxyr
 from lib.utils.renderer import render_png_raw
 
@@ -44,3 +44,39 @@ def ground_truths(image_name):
     filepath = os.path.join(GROUND_TRUTH_DIR, image_name + '.lxyr')
     gts = read_lxyr(filepath)
     return json.dumps([gt.__dict__ for gt in gts])
+
+
+@app.route('/images/<image_name>/ground_truths/<int:gt_id>')
+def ground_truth_single(image_name, gt_id):
+    """
+    Returns specified volcano image from specified main image_name
+    """
+
+    # First read all ground truths of this image
+    filepath = os.path.join(GROUND_TRUTH_DIR, image_name + '.lxyr')
+    gts = read_lxyr(filepath)
+    try:
+        gt = gts[gt_id]
+        # Use standard radius for all instances
+        gt.radius = VOLCANO_PIXEL_SIZE
+
+        # Extract image fragment
+        signal = read_signal(IMAGE_DIR, image_name)
+        rect = gt.get_rectangle()
+        gt_image = signal.extract_rectangle(rect[0], rect[1])
+
+        # Render
+        image_data = render_png_raw(gt_image)
+        response = make_response(image_data)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except:
+        abort(404)
+
+
+@app.route('/volcanoes')
+def all_volcanoes():
+    """
+    Display all existing ground truth images
+    """
+    return None
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 IMAGE_DIR = os.path.join(basedir, 'data/Images')
 GROUND_TRUTH_DIR = os.path.join(basedir, 'data/GroundTruths')
+VOLCANO_PIXEL_SIZE = 15
