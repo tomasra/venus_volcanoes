a982aa65ffc81885474ce2f6dda195acae317d9f
Tomas Raila <tomas.raa@gmail.com>
2014-05-18 18:05:37 +0300

    read lxyv data

--- lib/utils/reader.py
+++ lib/utils/reader.py
@@ -25,41 +25,98 @@ def read_signal(directory, signal_name):
     Creates signal object from .spr file
     and its corresponding .sdt file with signal data.
     """
-    try:
-        signal_filenames = _get_signal_filenames(directory, signal_name)
-        spr_path = signal_filenames['spr']
-        sdt_path = signal_filenames['sdt']
+    # try:
+    signal_filenames = _get_signal_filenames(directory, signal_name)
+    spr_path = signal_filenames['spr']
+    sdt_path = signal_filenames['sdt']
 
-        # Read .spr file
-        with open(spr_path, 'r') as f:
-            # num not needed at the moment
-            f.readline()
-            cols = int(f.readline())
-            # not needed
-            f.readline()
-            f.readline()
-            rows = int(f.readline())
-            # not needed
-            f.readline()
-            f.readline()
-            # dtype not needed at the moment
-            f.readline()
+    # Read .spr file
+    with open(spr_path, 'r') as f:
+        # num not needed at the moment
+        f.readline()
+        cols = int(f.readline())
+        # not needed
+        f.readline()
+        f.readline()
+        rows = int(f.readline())
+        # not needed
+        f.readline()
+        f.readline()
+        # dtype not needed at the moment
+        f.readline()
 
-        # Read .sdt file
-        with open(sdt_path, 'rb') as f:
-            data = [int(b) for b in bytearray(f.read())]
+    # Read .sdt file
+    with open(sdt_path, 'rb') as f:
+        data = [int(b) for b in bytearray(f.read())]
 
-        # Convert to numpy array
-        data = np.array(data).reshape((rows, cols)).astype(np.uint8)
+    # Convert to numpy array
+    data = np.array(data).reshape((rows, cols)).astype(np.uint8)
 
-        # Create the signal
-        signal = Image(
-            data=data,
-            name=signal_name)
-        return signal
-    except Exception:
-        # Files not found?
-        return None
+    # Create the signal
+    signal = Image(
+        data=data,
+        name=signal_name)
+    return signal
+    # except Exception:
+    #     # Files not found?
+    #     return None
+
+
+def read_spoiled_image(directory, image_name, spoil_size=2):
+    """
+    Special case for reading matched filter template images.
+    Their actual dimensions are twice as big as specified in .spr file,
+    so it's necessary to read the image in small blocks
+    (spoil_size X spoil_size) and take their averages.
+    """
+    image_filenames = _get_signal_filenames(directory, image_name)
+    spr_path = image_filenames['spr']
+    sdt_path = image_filenames['sdt']
+
+    # Read .spr file
+    with open(spr_path, 'r') as f:
+        # num not needed at the moment
+        f.readline()
+        cols = int(f.readline())
+        # not needed
+        f.readline()
+        f.readline()
+        rows = int(f.readline())
+        # not needed
+        f.readline()
+        f.readline()
+        # dtype not needed at the moment
+        f.readline()
+
+    # Read .sdt file
+    with open(sdt_path, 'rb') as f:
+        data = [int(b) for b in bytearray(f.read())]
+
+    # data_rows = rows * spoil_size
+    # data_cols = cols * spoil_size
+    # raw_data = np.array(data).reshape((data_rows, data_cols)).astype(np.uint8)
+
+    # spoiled_data = np.array([
+    #     int(np.rint(np.mean(raw_data[
+    #         row:row + spoil_size,
+    #         col:col + spoil_size])))
+    #     for col in xrange(0, data_cols, spoil_size)
+    #     for row in xrange(0, data_rows, spoil_size)
+    # ]).reshape(rows, cols).astype(np.uint8)
+
+    spoiled_data = np.array([
+        int(np.rint(np.mean(data[idx:idx + pow(spoil_size, 2)])))
+        for idx in xrange(0, len(data), pow(spoil_size, 2))
+    ]).reshape(rows, cols).astype(np.uint8)
+
+    # import pdb; pdb.set_trace()
+    # Convert to numpy array
+
+    # Create the signal
+    signal = Image(
+        data=spoiled_data,
+        name=image_name)
+    return signal
 
 
 def read_signals(directory, image_names=None):
@@ -80,11 +137,11 @@ def read_signals(directory, image_names=None):
         ]
 
 
-def read_lxyr(directory, signal_name):
+def read_lxyr(directory, image_name):
     """
     Reads a list of ground truths from lxyr file
     """
-    filepath = os.path.join(directory, signal_name + '.lxyr')
+    filepath = os.path.join(directory, image_name + '.lxyr')
     with open(filepath, 'r') as f:
         ground_truths = [
             _parse_lxyr_entry(line)
@@ -107,6 +164,22 @@ def read_lxyrs(directory):
     ))
 
 
+def read_lxyv(directory, image_name):
+    """
+    Reads FOA entries from lxyv file
+    """
+    filepath = os.path.join(directory, image_name + '.lxyv')
+    with open(filepath, 'r') as f:
+        ground_truths = [
+            _parse_lxyv_entry(line)
+            for line in f.readlines()
+            # Check if string is not empty
+            # (remove trailing whitespace with strip()).
+            if line.strip()
+        ]
+    return ground_truths
+
+
 def _parse_lxyr_entry(line):
     """
     Parses one line from .lxyr file and returns ground truth object.
@@ -116,6 +189,20 @@ def _parse_lxyr_entry(line):
         x=int(float(parts[1])),
         y=int(float(parts[2])),
         radius=float(parts[3]),
+        class_value=int(parts[0])
+    )
+
+
+def _parse_lxyv_entry(line):
+    """
+    Parses one line from .lxyv file and
+    returns GroundTruth object
+    """
+    parts = line.split()
+    return GroundTruth(
+        x=int(float(parts[1])),
+        y=int(float(parts[2])),
+        corr_value=float(parts[3]),
         class_value=int(parts[0])
     )
 
