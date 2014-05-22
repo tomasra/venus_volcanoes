3ccea827fd0a320f31ee36fb1835d42976198eb8
Tomas Raila <tomas.raa@gmail.com>
2014-05-22 12:52:48 +0300

    more general parameters for classifier methods

--- lib/algorithms/classifier.py
+++ lib/algorithms/classifier.py
@@ -1,27 +1,47 @@
 import mdp
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
+from config import VOLCANO_RADIUS
 
 
 class Classifier(object):
     def __init__(self):
         self.classifier = None
 
-    def train(self, vectors, classes):
+    def train(self, image_set, volcano_radius=VOLCANO_RADIUS):
         """
-        Trains classifier with provided vectors and their classes.
+        Training images should contain lists of known volcanoes
         """
+        vectors, classes = self._get_training_data(image_set, volcano_radius)
         self.classifier = GaussianNB()
         scaled_vectors = self._reduce_dimensions(vectors)
         self.classifier.fit(scaled_vectors, classes)
         return self
 
-    def run(self, vectors):
+    def run(self, image, ground_truths):
         """
         Runs classifier on the unseen list of vectors.
         """
+        vectors = [
+            gt_image.to_vector()
+            for gt_image in image.ground_truth_images(
+                radius=VOLCANO_RADIUS,
+                ground_truths=ground_truths)
+        ]
         scaled_vectors = self._reduce_dimensions(vectors)
         return self.classifier.predict(scaled_vectors)
+        # TODO: set these class values for passed ground truths
+
+    def _get_training_data(self, image_set, volcano_radius=8):
+        """
+        Extracts vectors/classes from training image set
+        """
+        vectors = [
+            gt_image.to_vector()
+            for gt_image in image_set.ground_truth_images(radius=volcano_radius)
+        ]
+        classes = image_set.ground_truth_classes()
+        return vectors, classes
 
     def _reduce_dimensions(
         self, vectors,
